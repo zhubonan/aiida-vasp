d15a8cc28097f8b3e6665af22c4a4f8f1f1ede09
Bonan Zhu <zhubonan@outlook.com>
2025-06-10 21:47:13 +0800

    Move to stream.py

--- src/aiida_vasp/parsers/content_parsers/stream.py
+++ src/aiida_vasp/parsers/content_parsers/stream.py
@@ -6,7 +6,10 @@ for VASP related notification, warnings, and errors.
 # pylint: disable=abstract-method
 import re
 
+from aiida import orm
+from aiida.common.exceptions import NotExistent
 from parsevasp.stream import Stream
+from tqdm import tqdm
 
 from aiida_vasp.parsers.content_parsers.base import BaseFileParser
 
@@ -125,3 +128,48 @@ class StreamParser(BaseFileParser):
 
         number_of_entries = len(self._content_parser)
         return number_of_entries
+
+
+def scan_for_vasp_errors(only_none_zero=False, print_yaml=False):
+    """
+    Scan uncaptured VASP errors
+    :param only_none_zero: Only print errors with exit_status != 0
+    :param print_yaml: Print the YAML representation of the errors
+    :return: A dictionary of errors
+    """
+
+    q = orm.QueryBuilder()
+    q.append(
+        orm.CalcJobNode,
+        filters={
+            'attributes.process_label': 'VaspCalculation',
+            'attributes.exit_status': {'!==': 0},
+        },
+        tag='calc',
+        project=['*', 'attributes.exit_status'],
+    )
+    q.append(orm.FolderData, tag='retrieved', project=['*'])
+    ntotal = q.count()
+    generic_errors = {}
+    for calc, exit_status, retrieved in tqdm(q.iterall(), total=ntotal):
+        if only_none_zero and exit_status == 0:
+            continue
+        try:
+            with retrieved.base.repository.open('vasp_output', 'r') as handle:
+                parser = Stream(file_handler=handle)
+        except NotExistent:
+            print(f'No vasp output found for {calc.uuid}')
+        for item in parser.entries:
+            if item.shortname == 'generic_box_error':
+                generic_errors[item.regex.pattern] = item
+    if print_yaml:
+        for item in generic_errors.values():
+            print(f'{item.shortname}:')
+            print(f'  kind: {item.kind}')
+            print(f'  location: {item.location}')
+            print(f'  regex: {item.regex.pattern}')
+            print(f'  message: {item.message}')
+            print(f'  recover: {item.recover}')
+            print(f'  suggestion: {item.suggestion}')
+
+    return generic_errors
